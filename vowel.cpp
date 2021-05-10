5bb6cccf4fb930b8037a2e2f922654fefa83417c
Bhupendrashahu <76242175+Bhupendrashahu@users.noreply.github.com>
2021-05-10 22:29:50 +0530

    Add files via upload

---
+++ vowel.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char n;
+	cout<<"enter the alphabet \n";
+    cin>>n;
+	switch(n)
+	{
+		    case 'a':
+		cout<<"vowel";
+		break;
+		    case 'e':
+		cout<<"vowel";
+		break;
+			case 'i':
+		cout<<"vowel";
+		break;
+			case 'o':
+		cout<<"vowel";
+		break;
+			case 'u':
+		cout<<"vowel";
+		break;
+	    	default:
+		cout<<"constant";
+	
+	}
+}
